744432e0c76e68e0e35ab8b9dca176c140d4622b
FedericoKCL <fed.deployment@aol.co.uk>
2016-01-22 11:00:35 +0000

    finished lesson 3a

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -1,3 +1,4 @@
+//  view controller is created when we made our project.
 //
 //  ViewController.swift
 //  Pitch Perfect
@@ -9,7 +10,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //uiLabel called "recordInpogress"
+    //IB = linked to the storyboard (Interfaee builder)
+    // weak = manage memory for our variables
+    // weak = someone else not me created that variable
+    // ! at the end = optional value for variables
+    @IBOutlet weak var recordInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
 
+
+    //skeleton button to record
+    //IB = interface builder
+    //IBAction tells me that this is not a normal function
+    //..but that is connected to the storyboard or the interface builder
+    @IBAction func recordAudio(sender: UIButton) {
+        recordInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+        //TODO: record the user voice
+    }
+    
+    
+    @IBAction func stopRecord(sender: UIButton) {
+        recordInProgress.hidden = true
+    }
+    
 
 }
 
