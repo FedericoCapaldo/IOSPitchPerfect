719560b573b1d1a3e7cbc11ef47cbfacd0434eec
FedericoKCL <fed.deployment@aol.co.uk>
2016-01-22 17:41:40 +0000

    now playing audio

--- Pitch Perfect/PlaySoundViewController.swift
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -7,21 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            let url = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: url, error:  nil)
+            println("all good")
+        } else {
+            println("the file path is empty")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
-
+    
+    @IBAction func playSlowSound(sender: AnyObject) {
+        audioPlayer.play()
+    }
+    
     /*
     // MARK: - Navigation
 
