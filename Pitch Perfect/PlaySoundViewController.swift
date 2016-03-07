3d6601454a552ae01b5c44538812dfac4a4676da
FedericoKCL <fed.deployment@aol.co.uk>
2016-03-07 00:49:36 +0000

    added darthvater. Project finished

--- Pitch Perfect/PlaySoundViewController.swift
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -14,10 +14,14 @@ class PlaySoundViewController: UIViewController {
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
     
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,7 +46,32 @@ class PlaySoundViewController: UIViewController {
     }
     
     @IBAction func playChipmunkSound(sender: AnyObject) {
-        // TODO: change pitch of the sound
+        playAudioWithVariablePitch(1000)
+    }
+    
+    @IBAction func playDarthvaderAudio(sender: AnyObject) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
     }
     
     @IBAction func stopPlaySound(sender: AnyObject) {
